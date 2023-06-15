b448e5a8226f4f5def891ef915acc440de7ecd56
Jason Brewer <jabrewer818@gmail.com>
2023-06-15 13:04:55 -0500

    cleanup

--- builder-settings.ts
+++ builder-settings.ts
@@ -2,8 +2,8 @@ import { Builder, builder } from "@builder.io/react";
 
 import './components/departments/department-card.builder';
 
-// const API_KEY_PUBLIC : string = process.env.BUILDER_PUBLIC_KEY;
-const API_KEY_PUBLIC : string = "073f4fc59c1e4b0d93c2c757e7f0e2e9";
+const API_KEY_PUBLIC : string = process.env.BUILDER_PUBLIC_KEY;
+// const API_KEY_PUBLIC : string = "073f4fc59c1e4b0d93c2c757e7f0e2e9";
 builder.init(API_KEY_PUBLIC);
 
 // Remove this to allow all built-in components to be used too
