33ad47b2d4a15054fcecc36fec2b6c1cac3be868
Jason Brewer <jabrewer818@gmail.com>
2023-06-21 10:13:21 -0500

    updated conditions url

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className={s.navbar}>
       <Link href="/" className={s.navbar__link}>Home</Link>
       <Link href="/home-example" className={s.navbar__link}>Home Example</Link>
-      <Link href="/conditions" className={s.navbar__link}>Conditions</Link>
+      <Link href="/conditions-example" className={s.navbar__link}>Conditions</Link>
       <Link href="/about" className={s.navbar__link}>About</Link>
     </nav>
   );
